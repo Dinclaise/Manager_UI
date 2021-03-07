30bb64adf38a827bd6e6e6d033b5b26bd63e0c72
Anton <Anohin.Anton@wildberries.ru>
2021-03-07 22:38:13 +0300

    refactoring code and added base-request-handler

--- src/Server/UsersHandler.ts
+++ src/Server/UsersHandler.ts
@@ -1,17 +1,15 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { HTTP_CODES, HTTP_METHODS } from "../Shared/Model";
 import { UsersDBAccess } from "../User/UserDBAccess";
+import { BaseRequestHandler } from "./BaseRequestHandler";
 import { Handler } from "./Model";
 import { Utils } from "./Utils";
 
-export class UsersHandler implements Handler {
-  private req: IncomingMessage;
-  private res: ServerResponse;
+export class UsersHandler extends BaseRequestHandler {
   private usersDBAccess: UsersDBAccess = new UsersDBAccess();
 
   constructor(req: IncomingMessage, res: ServerResponse) {
-    this.req = req;
-    this.res = res;
+    super(req, res);
   }
 
   async handleRequest(): Promise<void> {
@@ -28,10 +26,5 @@ export class UsersHandler implements Handler {
 
   private async handleGet() {
     const parsedUrl = Utils.getUrlParameters(this.req.url);
-  }
-
-  private async handleNotFound() {
-    this.res.statusCode = HTTP_CODES.NOT_FOUND;
-    this.res.write("not found");
   }
 }
