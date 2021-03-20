308e405962ea12eb7e93c6523694096763e4a9c3
Anton <Anohin.Anton@wildberries.ru>
2021-03-20 18:27:24 +0300

    beginning set a ui

---
+++ dist/Main.js
@@ -0,0 +1,7 @@
+export class Main {
+    constructor() {
+        console.log("Contructed new Instance of the program");
+    }
+}
+new Main();
+//# sourceMappingURL=Main.js.map
---
+++ src/Main.ts
@@ -0,0 +1,7 @@
+export class Main {
+  public constructor() {
+    console.log("Contructed new Instance of the program");
+  }
+}
+
+new Main();
