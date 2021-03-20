a481b6deb85d0ab29ec758ac0a93b74d8f4b28e9
Anton <Anohin.Anton@wildberries.ru>
2021-03-20 19:07:54 +0300

    install packages and webpack

--- dist/Main.js
+++ dist/Main.js
@@ -1,7 +1,12 @@
+import { Router } from "./Router";
 export class Main {
     constructor() {
+        this.router = new Router();
         console.log("Contructed new Instance of the program");
     }
+    launchApp() {
+        this.router.handleRequest();
+    }
 }
-new Main();
+new Main().launchApp();
 //# sourceMappingURL=Main.js.map
--- src/Main.ts
+++ src/Main.ts
@@ -1,7 +1,15 @@
+import { Router } from "./Router";
+
 export class Main {
+  private router: Router = new Router();
+
   public constructor() {
     console.log("Contructed new Instance of the program");
   }
+
+  public launchApp() {
+    this.router.handleRequest();
+  }
 }
 
-new Main();
+new Main().launchApp();
